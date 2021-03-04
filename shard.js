5e9ff24d64a94cd74e08935d24ae85d374866fdf
Dracora <mambachromarazer@gmail.com>
2021-03-04 15:55:27 -0500

    Delete shard.js

--- shard.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const { ShardingManager } = require('discord.js');
-
-const { DISCORD_TOKEN } = process.env;
-
-const manager = new ShardingManager('./index.js', { token: DISCORD_TOKEN, autoSpawn: true, respawn: true });
-
-manager.spawn();
